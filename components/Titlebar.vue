f4dddee3b9a0c994dff138aedb15b39c24c659b2
fumikoyama <co.fumi@gmail.com>
2019-09-23 22:50:20 +0900

    タイトルの表示方法を変更

---
+++ components/Titlebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container has-text-centered">
+    <h1 class="title">
+      <b-icon :icon="pages[$route.name].icon"></b-icon>
+      {{ pages[$route.name].title }}
+    </h1>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pages: {
+        index: { title: 'ホーム', icon: 'home' },
+        todos: { title: 'Todos', icon: 'format-list-checkbox' },
+        performance: { title: '実績', icon: 'gauge' },
+        settings: { title: '設定', icon: 'tune' }
+      }
+    }
+  }
+}
+</script>
--- pages/performance/index.vue
+++ pages/performance/index.vue
@@ -1,49 +1,36 @@
 <template>
-  <section class="section hero">
-    <header class="hero-head">
-      <div class="container has-text-centered">
-        <h1 class="title">
-          <b-icon icon="format-list-checkbox"></b-icon>
-          実績
-        </h1>
-        <hr />
-      </div>
-    </header>
-    <section class="hero-body">
-      <div class="container">
-        <div class="box">
-          {{ `完了したポモ回数：${totalPomoCount}` }}
-        </div>
-        <div class="box">
-          {{ `完了したポモ時間：${totalElapsed}` }}
-        </div>
-        <div class="box">
-          {{ `中断された回数：${totalDisturbedCount}` }}
-        </div>
-        <div class="box">
-          {{ `中断された時間：${totalDisturbed}` }}
-        </div>
-        <div class="box">
-          {{ `超過した時間：${totalOverTime}` }}
-        </div>
-        <div class="box">
-          {{ `休憩回数：${totalBreakCount}` }}
-        </div>
-        <div class="box">
-          {{ `休憩時間：${totalBreakTime}` }}
-        </div>
-        <b-button
-          class="is-fullwidth"
-          type="is-link"
-          outlined
-          :disabled="!canClear"
-          @click="clear"
-        >
-          クリア
-        </b-button>
-      </div>
-    </section>
-  </section>
+  <div class="container">
+    <div class="box">
+      {{ `完了したポモ回数：${totalPomoCount}` }}
+    </div>
+    <div class="box">
+      {{ `完了したポモ時間：${totalElapsed}` }}
+    </div>
+    <div class="box">
+      {{ `中断された回数：${totalDisturbedCount}` }}
+    </div>
+    <div class="box">
+      {{ `中断された時間：${totalDisturbed}` }}
+    </div>
+    <div class="box">
+      {{ `超過した時間：${totalOverTime}` }}
+    </div>
+    <div class="box">
+      {{ `休憩回数：${totalBreakCount}` }}
+    </div>
+    <div class="box">
+      {{ `休憩時間：${totalBreakTime}` }}
+    </div>
+    <b-button
+      class="is-fullwidth"
+      type="is-link"
+      outlined
+      :disabled="!canClear"
+      @click="clear"
+    >
+      クリア
+    </b-button>
+  </div>
 </template>
 
 <script>
