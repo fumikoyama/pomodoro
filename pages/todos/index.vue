b9bb5f48d69a30b789350c01f8546c59b46de55b
fumikoyama <co.fumi@gmail.com>
2019-09-25 03:52:00 +0900

    TODOのゴミ箱とそれ以外でタブ表示に変更

--- pages/todos/index.vue
+++ pages/todos/index.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="container">
-    <!-- 完了リスト -->
-    <todo-completed-list />
-    <!-- 未完了リスト -->
-    <todo-incomplete-list />
-    <!-- 追加/編集 -->
-    <todo-editor />
-    <!-- ゴミ箱 -->
-    <todo-trash />
-  </div>
+  <b-tabs position="is-centered" type="is-toggle">
+    <b-tab-item>
+      <template slot="header">
+        <b-icon icon="view-list" size="is-small" />
+      </template>
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <!-- 完了リスト -->
+            <todo-completed-list />
+            <!-- 未完了リスト -->
+            <todo-incomplete-list />
+          </div>
+          <!-- 追加/編集 -->
+          <todo-editor class="column" />
+        </div>
+      </div>
+    </b-tab-item>
+    <b-tab-item>
+      <template slot="header">
+        <b-icon icon="delete" size="is-small" />
+      </template>
+      <div class="container">
+        <!-- ゴミ箱 -->
+        <todo-trash />
+      </div>
+    </b-tab-item>
+  </b-tabs>
 </template>
 
 <script>
